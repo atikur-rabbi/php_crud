fd2463fffba3bc108989c5dc655790ca7961f8f9
atikur-rabbi <msawon94@gmail.com>
2020-08-25 19:54:43 +0600

    Updated at 2020-08-25_0754 PM

--- api/demo.php
+++ api/demo.php
@@ -1,13 +1,23 @@
 <?php
-   class MyDB extends SQLite3 {
-      function __construct() {
-         $this->open('test.db');
-      }
-   }
+
    $db = new MyDB();
-   if(!$db) {
-      echo $db->lastErrorMsg();
-   } else {
-      echo "Opened database successfully\n";
-   }
+
+   $db = new SQLite3('/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+
+   $db->query('CREATE TABLE IF NOT EXISTS "visits" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "url" VARCHAR,
+    "time" DATETIME
+)');
+
+$statement = $db->prepare('INSERT INTO "visits" ("url", "time") VALUES (:url, :time)');
+$statement->bindValue(':url', ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'https') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+$statement->bindValue(':time', date('Y-m-d H:i:s'));
+$statement->execute();
+
+$visits = $db->querySingle('SELECT COUNT(id) FROM "visits"');
+
+echo("User visits: $visits");
+
+$db->close();
 ?>
